9cbdb0a14ca3c1db5802ed643caa49c950f4dd32
刘云霞 <liuyunxia@liuyunxiadeMac-mini.local>
2017-11-24 09:38:01 +0800

    添加适配

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { WebView, View, StyleSheet } from 'react-native';
+import { WebView, View, StyleSheet, Platform } from 'react-native';
 import renderChart from './renderChart';
 import echarts from './echarts.min';
+
+const source = (Platform.OS == 'ios') ? require('./tpl.html') : {'uri':'file:///android_asset/tpl.html'}
 
 export default class App extends Component {
   componentWillReceiveProps(nextProps) {
@@ -16,11 +18,12 @@ export default class App extends Component {
         <WebView
           ref="chart"
           scrollEnabled = {false}
+          scalesPageToFit={Platform.OS === 'ios' ? false : true}
           injectedJavaScript = {renderChart(this.props)}
           style={{
             height: this.props.height || 400,
           }}
-          source={require('./tpl.html')}
+          source={source}
         />
       </View>
     );
